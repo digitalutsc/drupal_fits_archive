f5706c7865a6f275512f70074c0e64fd0236b2ef
Kyle Huynh <kyle.huynh205@gmail.com>
2021-06-05 21:29:49 -0400

    Fits ops is integrated with Advanced queue and works

--- src/Plugin/Action/FitsAction.php
+++ src/Plugin/Action/FitsAction.php
@@ -4,7 +4,9 @@ namespace Drupal\fits\Plugin\Action;
 
 use Drupal\Core\Action\ActionBase;
 use Drupal\Core\Session\AccountInterface;
-use GuzzleHttp\Client;
+use Drupal\advancedqueue\Entity\Queue;
+use Drupal\advancedqueue\Job;
+
 
 /**
  * Provides a 'FitsAction' action.
@@ -30,58 +32,20 @@ class FitsAction extends ActionBase {
   public function execute($file = NULL)
   {
     /** @var \Drupal\file\FileInterface $file */
-    if (!isset($file)) {
-      return;
-    }
-    // extract Fits from xml
-    $fits_xml = $this->getFits($file);
-    $fits = simplexml_load_string($fits_xml);
-    $fit_json = json_encode($fits);
-
-    // store the whole fits to json field, use either one of theme. Choose after determine which one is better.
-    $file->field_fits->setValue($fit_json);
-
-    $fits = json_decode($fit_json);
-
-    foreach ($file->getFields() as $field) {
-      if ($field->getFieldDefinition()->getType() === "string" && strpos($field->getFieldDefinition()->getName(), "_fits_") !== false)  {
-        $field->setValue(jmesPathSearch(getJmespath($field->getFieldDefinition()->getDescription()), $fits));
-      }
-    }
-
-    // extract selective fields and save to other fields
-    $file->save();
-  }
-
-
-  /**
-   * Rest call to Fits
-   * @param \Drupal\file\Entity\File $file
-   * @return string
-   */
-  function getFits(\Drupal\file\Entity\File $file)
-  {
     $config = \Drupal::config('fits.fitsconfig');
-    try {
-      $options = [
-        'base_uri' => $config->get("fits-server-url")
-      ];
-      $client = new Client($options);
-      $response = $client->post($config->get("fits-server-endpoint"), [
-        'multipart' => [
-          [
-            'name' => 'datafile',
-            'filename' => $file->label(),
-            'contents' => file_get_contents($file->getFileUri()),
-          ],
-        ]
-      ]);
+    // Create a job and add to Advanced Queue
+    $payload = [
+      'fid' => $file->id(),
+      'type' => $file->getEntityTypeId(),
+      'action' => "extract_Fits"
+    ];
 
-      return $response->getBody()->getContents();
-    } catch (Exception $e) {
-      return null;
+    $job = Job::create('fits_job', $payload);
+    if ($job instanceof Job) {
+      $q = Queue::load($config->get("fits-advancedqueue_id"));
+      $q->enqueueJob($job);
     }
-
   }
+
 
 }
--- src/Plugin/AdvancedQueue/JobType/FitsJob.php
+++ src/Plugin/AdvancedQueue/JobType/FitsJob.php
@@ -3,8 +3,9 @@
 namespace Drupal\fits\Plugin\AdvancedQueue\JobType;
 
 use Drupal\advancedqueue\Job;
-use Drupal\advancedqueue\JobResult;
 use Drupal\advancedqueue\Plugin\AdvancedQueue\JobType\JobTypeBase;
+use GuzzleHttp\Client;
+use Drupal\advancedqueue\JobResult;
 
 /**
  * @AdvancedQueueJobType(
@@ -19,9 +20,102 @@ class FitsJob extends JobTypeBase
   {
     // TODO: Implement process() method.
     try {
+      $payload = $job->getPayload();
+      /** @var \Drupal\file\FileInterface $file */
+      $file = \Drupal\file\Entity\File::load($payload['fid']);
+      $result = $this->extractFits($file);
+
+      if ($result['result'] === true) {
+        return JobResult::success(t($result['outcome']));
+      }else {
+        return JobResult::failure(t($result['outcome']));
+      }
 
     } catch (\Exception $e) {
+      return JobResult::failure($e->getMessage());
+    }
+  }
 
+
+  /**
+   * Extract Fits
+   * @param null $file
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Drupal\Core\TypedData\Exception\ReadOnlyException
+   */
+  public function extractFits($file = NULL) {
+    /** @var \Drupal\file\FileInterface $file */
+    $report = "";
+    $sucess = true;
+    if (!isset($file)) {
+      return;
+    }
+    // extract Fits from xml
+    $fits_result = $this->getFits($file);
+
+    if ($fits_result['code'] === 500) {
+      $report .= '<p>Get Fits XML: ' . $fits_result['message']. "</p>";
+      return ['result' => false, "outcome" => $report];
+    }
+
+    $report .=  "<p>Get Fits XML: ". $fits_result['message']. "</p>";
+    $fits_xml = $fits_result['output'];
+    $fits = simplexml_load_string($fits_xml);
+    $fit_json = json_encode($fits);
+
+    // store the whole fits to json field, use either one of theme. Choose after determine which one is better.
+    $file->field_fits->setValue($fit_json);
+
+    $fits = json_decode($fit_json);
+
+    foreach ($file->getFields() as $field) {
+      if ($field->getFieldDefinition()->getType() === "string" && strpos($field->getFieldDefinition()->getName(), "_fits_") !== false)  {
+        $extractedFitsValue = jmesPathSearch(getJmespath($field->getFieldDefinition()->getDescription()), $fits);
+        if (empty($extractedFitsValue)) {
+          $report .= "<p>Extract Fits: JMESPath for the field - <code>" .$field->getName(). "</code> seems to be invalid</p>";
+          $sucess = false;
+        }
+        else {
+          $report .= "<p>Extract Fits: Field  <code>" .$field->getName(). ".value = $extractedFitsValue</code>.</p>";
+        }
+        $field->setValue($extractedFitsValue, $fits);
+      }
+    }
+
+    // extract selective fields and save to other fields
+    $file->save();
+    return ['result' => $sucess, "outcome" => $report];
+  }
+
+  /**
+   * Rest call to Fits
+   * @param \Drupal\file\Entity\File $file
+   * @return string
+   */
+  function getFits(\Drupal\file\Entity\File $file)
+  {
+    $config = \Drupal::config('fits.fitsconfig');
+    try {
+      $options = [
+        'base_uri' => $config->get("fits-server-url")
+      ];
+      $client = new Client($options);
+      $response = $client->post($config->get("fits-server-endpoint"), [
+        'multipart' => [
+          [
+            'name' => 'datafile',
+            'filename' => $file->label(),
+            'contents' => file_get_contents($file->getFileUri()),
+          ],
+        ]
+      ]);
+      return [
+        "code" => 200,
+        "message" => "Get Fits Technical Metadata successfully",
+        'output' => $response->getBody()->getContents()
+      ];
+    } catch (\Exception $e) {
+      return ["code" => 500, 'message' => $e->getMessage()];
     }
   }
 }
