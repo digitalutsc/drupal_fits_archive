0c08a7eaf87363935e1d255d4e0f5bbafd5b0995
Kyle Huynh <kyle.huynh205@gmail.com>
2021-06-15 11:37:13 -0400

    PRONOM field and its handler added and works

--- src/Plugin/AdvancedQueue/JobType/FitsJob.php
+++ src/Plugin/AdvancedQueue/JobType/FitsJob.php
@@ -81,11 +81,50 @@ class FitsJob extends JobTypeBase
         }
         $field->setValue($extractedFitsValue, $fits);
       }
+      else if ($field->getFieldDefinition()->getName() === "field_fits_pronom_puid") {
+        $extractedFitsValue = jmesPathSearch(getJmespath($field->getFieldDefinition()->getDescription()), $fits);
+        if (empty($extractedFitsValue)) {
+          $report .= "<p>Extract Fits: JMESPath for the field - <code>" . $field->getName() . "</code> seems to be invalid</p>";
+          $sucess = false;
+        } else {
+          $report .= "<p>Extract Fits: Field  <code>" . $field->getName() . ".value = $extractedFitsValue</code>.</p>";
+        }
+        // search PRONOM fitst
+
+        $tid = $this->searchPRONOM($extractedFitsValue);
+        if ($tid === -1) {
+          $tid = \Drupal\taxonomy\Entity\Term::create([
+            'name' => $extractedFitsValue,
+            'vid' => "pronom",
+          ])->save();
+          $tid = $this->searchPRONOM($extractedFitsValue);
+        }
+        $field->setValue($tid);
+      }
     }
 
     // extract selective fields and save to other fields
     $file->save();
     return ['result' => $sucess, "outcome" => $report];
+  }
+
+  /**
+   * Search PRONOM  term
+   * @param String $pronom
+   * @return int
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  function searchPRONOM(String $pronom) {
+    $tid = -1;
+    $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree("pronom");
+    foreach ($terms as $term) {
+      if ($pronom === $term->name) {
+        $tid = $term->tid;
+        break;
+      }
+    }
+    return $tid;
   }
 
   /**
@@ -124,15 +163,14 @@ class FitsJob extends JobTypeBase
         $fits_path = $config->get("fits-path");
         //$cmd = $fits_path . " -i " . \Drupal::service('file_system')->realpath((ctype_space($file->getFileUri())) ? escapeshellarg($file->getFileUri()) : $file->getFileUri());
 
-	if (strpos($file->getFileUri(), ' ') !== false ) {
-           $file_path = \Drupal::service('file_system')->realpath("public://" . escapeshellarg($file->getFilename()));
-        }
-        else {
+        if (strpos($file->getFileUri(), ' ') !== false) {
+          $file_path = \Drupal::service('file_system')->realpath("public://" . escapeshellarg($file->getFilename()));
+        } else {
           $file_path = \Drupal::service('file_system')->realpath($file->getFileUri());
         }
-        $cmd = $fits_path . " -i " . $file_path;        
+        $cmd = $fits_path . " -i " . $file_path;
 
-	$xml = `$cmd`;
+        $xml = `$cmd`;
 
         //$messenger = \Drupal::messenger();
         //$messenger->addMessage($cmd, $messenger::TYPE_WARNING);
