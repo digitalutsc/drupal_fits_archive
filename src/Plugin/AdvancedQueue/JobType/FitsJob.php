a0b068fa5ae0aca7a6400e7923be363b2dcc4e77
Kyle Huynh <kyle.huynh@utoronto.ca>
2021-10-25 16:31:16 -0400

    Adjust code toward PHP codesniffer Drupal standard

--- src/Plugin/AdvancedQueue/JobType/FitsJob.php
+++ src/Plugin/AdvancedQueue/JobType/FitsJob.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\fits\Plugin\AdvancedQueue\JobType;
 
+use Drupal\taxonomy\Entity\Term;
+use Drupal\file\Entity\File;
 use Drupal\advancedqueue\Job;
 use Drupal\advancedqueue\Plugin\AdvancedQueue\JobType\JobTypeBase;
 use GuzzleHttp\Client;
@@ -13,56 +15,53 @@ use Drupal\advancedqueue\JobResult;
  *   label = @Translation("Fits"),
  * )
  */
-class FitsJob extends JobTypeBase
-{
+class FitsJob extends JobTypeBase {
 
-  public function process(Job $job)
-  {
-    // TODO: Implement process() method.
+  /**
+   * Implements process().
+   */
+  public function process(Job $job) {
     try {
       $payload = $job->getPayload();
 
-      // set retry config
+      // Set retry config.
       $this->pluginDefinition['max_retries'] = $payload['max_tries'];
       $this->pluginDefinition['retry_delay'] = $payload['retry_delay'];
 
       /** @var \Drupal\file\FileInterface $file */
-      $file = \Drupal\file\Entity\File::load($payload['fid']);
+      $file = File::load($payload['fid']);
       $result = $this->extractFits($file);
 
-      if ($result['result'] === true) {
-        return JobResult::success(t($result['outcome']));
-      } else {
-        return JobResult::failure(t($result['outcome']));
+      if ($result['result'] === TRUE) {
+        return JobResult::success($this->t($result['outcome']));
+      }
+      else {
+        return JobResult::failure($this->t($result['outcome']));
       }
 
-    } catch (\Exception $e) {
+    }
+    catch (\Exception $e) {
       return JobResult::failure($e->getMessage());
     }
   }
 
-
   /**
-   * Extract Fits
-   * @param null $file
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   * @throws \Drupal\Core\TypedData\Exception\ReadOnlyException
+   * Extract Fits.
    */
-  public function extractFits($file = NULL)
-  {
+  public function extractFits($file = NULL) {
     /** @var \Drupal\file\FileInterface $file */
     $config = \Drupal::config('fits.fitsconfig');
     $report = "";
-    $sucess = true;
+    $sucess = TRUE;
     if (!isset($file)) {
       return;
     }
-    // extract Fits from xml
+    // Extract Fits from xml.
     $fits_result = $this->getFits($file);
 
     if ($fits_result['code'] === 500) {
       $report .= 'Get Fits XML: ' . $fits_result['message'] . "\n";
-      return ['result' => false, "outcome" => $report];
+      return ['result' => FALSE, "outcome" => $report];
     }
 
     $report .= "Get Fits XML: " . $fits_result['message'] . "\n";
@@ -71,42 +70,44 @@ class FitsJob extends JobTypeBase
     $fits = simplexml_load_string($fits_xml);
     $fit_json = json_encode($fits);
 
-    // store the whole fits to json field, use either one of theme. Choose after determine which one is better.
+    // Store the whole fits to json field.
     $file->field_fits->setValue($fit_json);
 
     $fits = json_decode($fit_json);
     foreach ($file->getFields() as $field) {
-      if ($field->getFieldDefinition()->getType() === "string" && strpos($field->getFieldDefinition()->getName(), "_fits_") !== false) {
+      if ($field->getFieldDefinition()->getType() === "string" && strpos($field->getFieldDefinition()->getName(), "_fits_") !== FALSE) {
         $extractedFitsValue = $this->jmesPathSearch($this->getJmespath($field->getFieldDefinition()->getDescription()), $fits);
         if (empty($extractedFitsValue)) {
           $report .= "Extract Fits: JMESPath for the field - " . $field->getName() . " seems to be invalid\n";
-            if (in_array($field->getFieldDefinition()->getName(), $config->get("fits-default-fields"))) {
-                $sucess = false;
-            }
-        } else {
+          if (in_array($field->getFieldDefinition()->getName(), $config->get("fits-default-fields"))) {
+            $sucess = FALSE;
+          }
+        }
+        else {
           $report .= "Extract Fits: Field  " . $field->getName() . ".value = $extractedFitsValue.\n";
         }
         $field->setValue($extractedFitsValue, $fits);
       }
-      else if ($field->getFieldDefinition()->getName() === "field_fits_pronom_puid") {
+      elseif ($field->getFieldDefinition()->getName() === "field_fits_pronom_puid") {
         $extractedFitsValue = $this->jmesPathSearch($this->getJmespath($field->getFieldDefinition()->getDescription()), $fits);
         if (empty($extractedFitsValue)) {
           $report .= "Extract Fits: JMESPath for the field - " . $field->getName() . " seems to be invalid\n";
-            if (in_array($field->getFieldDefinition()->getName(), $config->get("fits-default-fields"))) {
-                $sucess = false;
-            }
-        } else {
+          if (in_array($field->getFieldDefinition()->getName(), $config->get("fits-default-fields"))) {
+            $sucess = FALSE;
+          }
+        }
+        else {
           $report .= "Extract Fits: Field  " . $field->getName() . ".value = $extractedFitsValue.\n";
         }
-        // search PRONOM fitst
+        // Search PRONOM fitst.
         if (!empty($extractedFitsValue)) {
-          $tid = $this->searchPRONOM($extractedFitsValue);
+          $tid = $this->searchPronom($extractedFitsValue);
           if ($tid === -1) {
-            $tid = \Drupal\taxonomy\Entity\Term::create([
+            $tid = Term::create([
               'name' => $extractedFitsValue,
               'vid' => "pronom",
             ])->save();
-            $tid = $this->searchPRONOM($extractedFitsValue);
+            $tid = $this->searchPronom($extractedFitsValue);
           }
           $field->setValue($tid);
         }
@@ -114,25 +115,21 @@ class FitsJob extends JobTypeBase
       }
     }
 
-    // extract selective fields and save to other fields
+    // Extract selective fields and save to other fields.
     $file->save();
     return ['result' => $sucess, "outcome" => $report];
   }
 
-
   /**
-   * Analyze field's description text and get Jmespath
-   *
-   * @param $jmespath
-   * @return mixed|string[]
+   * Analyze field's description text and get Jmespath.
    */
-  function getJmespath($desc) {
+  public function getJmespath($desc) {
     preg_match_all("/\[\{(.*?)\}\]/", $desc, $matches);
     $jmespath = $matches[1];
-    if (is_array($jmespath) && count($jmespath)> 1) {
+    if (is_array($jmespath) && count($jmespath) > 1) {
       return $jmespath;
     }
-    else if (is_array($jmespath) && count($jmespath)== 1) {
+    elseif (is_array($jmespath) && count($jmespath) == 1) {
       return $jmespath[0];
     }
     else {
@@ -142,10 +139,9 @@ class FitsJob extends JobTypeBase
   }
 
   /**
-   * From Jmespath(s), Get value of field from Fits json
-   * @param $path is Jmespath but can set as either a string or array
+   * From Jmespath(s), Get value of field from Fits json.
    */
-  function jmesPathSearch($path, $fits) {
+  public function jmesPathSearch($path, $fits) {
     if (is_array($path)) {
       $value = "";
       foreach ($path as $p) {
@@ -160,16 +156,12 @@ class FitsJob extends JobTypeBase
   }
 
   /**
-   * Search PRONOM  term
-   * @param String $pronom
-   * @return int
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * Search PRONOM term.
    */
-  function searchPRONOM($pronom) {
+  public function searchPronom($pronom) {
     $tid = -1;
     if (isset($pronom)) {
-      $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree("pronom");
+      $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree("pronom");
       foreach ($terms as $term) {
         if ($pronom === $term->name) {
           $tid = $term->tid;
@@ -181,17 +173,14 @@ class FitsJob extends JobTypeBase
   }
 
   /**
-   * Rest call to Fits
-   * @param \Drupal\file\Entity\File $file
-   * @return array
+   * Rest call to Fits.
    */
-  function getFits(\Drupal\file\Entity\File $file)
-  {
+  public function getFits(File $file) {
     $config = \Drupal::config('fits.fitsconfig');
     if ($config->get("fits-method") === "remote") {
       try {
         $options = [
-          'base_uri' => $config->get("fits-server-url")
+          'base_uri' => $config->get("fits-server-url"),
         ];
         $client = new Client($options);
         $response = $client->post($config->get("fits-server-url"), [
@@ -201,59 +190,60 @@ class FitsJob extends JobTypeBase
               'filename' => $file->label(),
               'contents' => file_get_contents($file->getFileUri()),
             ],
-          ]
+          ],
         ]);
         if (isset($response)) {
-            return [
-                "code" => 200,
-                "message" => "Get Fits Technical Metadata successfully",
-                'output' => $response->getBody()->getContents()
-            ];
+          return [
+            "code" => 200,
+            "message" => "Get Fits Technical Metadata successfully",
+            'output' => $response->getBody()->getContents(),
+          ];
         }
         else {
-            return [
-                "code" => 417,
-                "message" => "Failed Get Fits Technical Metadata.",
-                'output' => $response->getBody()->getContents()
-            ];
+          return [
+            "code" => 417,
+            "message" => "Failed Get Fits Technical Metadata.",
+            'output' => $response->getBody()->getContents(),
+          ];
         }
 
-      } catch (\Exception $e) {
+      }
+      catch (\Exception $e) {
         return ["code" => 500, 'message' => $e->getMessage()];
       }
-    } else {
+    }
+    else {
       try {
         $fits_path = $config->get("fits-path");
-        //$cmd = $fits_path . " -i " . \Drupal::service('file_system')->realpath((ctype_space($file->getFileUri())) ? escapeshellarg($file->getFileUri()) : $file->getFileUri());
-
-        if (strpos($file->getFilename(), ' ') !== false) {
-          $file_path = str_replace($file->getFilename(),  escapeshellarg($file->getFilename()), $file->getFileUri());
+        if (strpos($file->getFilename(), ' ') !== FALSE) {
+          $file_path = str_replace($file->getFilename(), escapeshellarg($file->getFilename()), $file->getFileUri());
           $file_path = \Drupal::service('file_system')->realpath($file_path);
-        } else {
+        }
+        else {
           $file_path = \Drupal::service('file_system')->realpath($file->getFileUri());
         }
         $cmd = $fits_path . " -i " . $file_path;
         $xml = `$cmd`;
-        //drupal_log($cmd);
+        // drupal_log($cmd);
         if (isset($xml)) {
-            return [
-                "code" => 200,
-                "message" => "Get Fits Technical Metadata successfully",
-                'output' => $xml
-            ];
+          return [
+            "code" => 200,
+            "message" => "Get Fits Technical Metadata successfully",
+            'output' => $xml,
+          ];
         }
         else {
-            return [
-                "code" => 417,
-                "message" => "Failed to get Fits Technical Metadata.",
-                'output' => $xml
-            ];
+          return [
+            "code" => 417,
+            "message" => "Failed to get Fits Technical Metadata.",
+            'output' => $xml,
+          ];
         }
-
-      } catch (\Exception $e) {
+      }
+      catch (\Exception $e) {
         return ["code" => 500, 'message' => $e->getMessage()];
       }
     }
-
   }
+
 }
